0bd77ed184c2fe734b160953d93fcdbad4817538
Евгений Минин <evgenminin1988@mail.ru>
2023-04-20 11:40:31 +0300

    'completed homework-1'

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,18 @@
 """Здесь надо написать тесты с использованием unittest для модуля stack."""
+import unittest
+
+from src.stack import Stack
+
+
+class TestStack(unittest.TestCase):
+
+    def test_push(self):
+        self.stack = Stack()
+        self.stack.push("data1")
+        self.stack.push("data2")
+        self.assertEqual(self.stack.top.data, "data2")
+        self.assertEqual(self.stack.top.next_node.data, "data1")
+
+
+if __name__ == '__main__':
+    unittest.main()
