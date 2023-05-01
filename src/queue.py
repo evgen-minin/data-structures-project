64006888c39e1fc1bae537e82eb3ad76a98196ab
Евгений Минин <evgenminin1988@mail.ru>
2023-05-02 01:54:19 +0300

    'completed homework-4'

--- src/queue.py
+++ src/queue.py
@@ -37,8 +37,15 @@ class Queue:
         Метод для удаления элемента из очереди. Возвращает данные удаленного элемента
 
         :return: данные удаленного элемента
-        """
-        pass
+        # """
+        if self.head is None:
+            return None
+        data = self.head.data
+        self.head = self.head.next_node
+        if self.head is None:
+            self.tail = None
+        return data
+
 
     def __str__(self):
         """Магический метод для строкового представления объекта"""
--- tests/test_queue.py
+++ tests/test_queue.py
@@ -6,8 +6,10 @@ from src.queue import Queue
 
 class TestQueue(unittest.TestCase):
 
-    def test_enqueue(self):
+    def setUp(self):
         self.queue = Queue()
+
+    def test_enqueue(self):
         self.queue.enqueue("data1")
         self.queue.enqueue("data2")
         self.queue.enqueue("data3")
@@ -17,9 +19,17 @@ class TestQueue(unittest.TestCase):
         self.assertEqual(self.queue.tail.next_node, None)
 
     def test_str(self):
-        self.queue = Queue()
         self.assertEqual(str(Queue()), "")
         self.queue.enqueue("data1")
         self.queue.enqueue("data2")
         self.queue.enqueue("data3")
         self.assertEqual(str(self.queue), "data1\ndata2\ndata3")
+
+    def test_dequeue(self):
+        self.queue.enqueue("data1")
+        self.queue.enqueue("data2")
+        self.queue.enqueue("data3")
+        self.assertEqual(self.queue.dequeue(), 'data1')
+        self.assertEqual(self.queue.dequeue(), 'data2')
+        self.assertEqual(self.queue.dequeue(), 'data3')
+        self.assertIsNone(self.queue.dequeue(), None)
